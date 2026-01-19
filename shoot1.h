43e124c9e7af6ca67eb2957c0b06d3dc62124b09
oliburr10 <aob100902@gmail.com>
2026-01-19 12:48:36 +0100

    Update shoot1.h

--- shoot1.h
+++ shoot1.h
@@ -2,6 +2,7 @@
 #define SHOOT_H
 
 #include <stdint.h>
+#include "players.h"
 #include "physics.h"
 
 #define MAX_BULLETS 20
@@ -15,11 +16,14 @@ typedef struct {
     int32_t x, y;
     BulletType_t type;
     int32_t player;
-
-    PhysBullet_t phys;   // <-- REQUIRED
+    PhysBullet_t phys;
 } Bullet_t;
 
+extern Bullet_t bullets[MAX_BULLETS];
+extern int bullet_count;
+
 void Bullets_Update(void);
-void Bullet_Spawn(int32_t shooter, struct P1 *p1, struct P2 *p2);
+void Bullet_Spawn(int32_t shooter, P1 *p1, P2 *p2);
 
 #endif
+
