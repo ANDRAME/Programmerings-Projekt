51cce86cb2c1cd7fc29261a9a3d7235b8cac65fa
oliburr10 <aob100902@gmail.com>
2026-01-20 15:39:07 +0100

    Create powerup.h

---
+++ powerup.h
@@ -0,0 +1,23 @@
+#ifndef POWERUP_H_
+#define POWERUP_H_
+
+#include <stdint.h>
+#include "players.h"
+#include "timer.h"
+
+typedef enum {
+    POWERUP_NONE = 0,
+    POWERUP_TRIPLE_SHOT = 1,   /* purple */
+    POWERUP_AUTO_FAST   = 2    /* yellow */
+} PowerupType_t;
+
+void powerups_init(void);
+
+/* Call every frame/tick */
+void powerups_update(P1 *p1, P2 *p2, Bounds *bounds);
+
+/* Effects */
+uint8_t powerup_triple_active(uint8_t player);
+uint8_t powerup_autofast_active(uint8_t player);
+
+#endif /* POWERUP_H_ */
