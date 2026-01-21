98a09658c42be3666c6a196503b57868f71d1397
elinakheirkhah-cyber <elinakheirkhah@gmail.com>
2026-01-21 11:07:46 +0100

    Add header file for game end screen functions

---
+++ gameEnd.h
@@ -0,0 +1,19 @@
+/*
+ * gameEnd.h
+ *
+ *  Created on: 20. jan. 2026
+ *      Author: elina
+ */
+
+#ifndef GAMEEND_H_
+#define GAMEEND_H_
+
+void endScreenG(uint8_t x, uint8_t y);
+void endScreenA(uint8_t x, uint8_t y);
+void endScreenM(uint8_t x, uint8_t y);
+void endScreenE(uint8_t x, uint8_t y);
+void endScreenE_1(uint8_t x, uint8_t y);
+void endScreenN(uint8_t x, uint8_t y);
+void endScreenED(uint8_t x, uint8_t y);
+
+#endif /* GAMEEND_H_ */
