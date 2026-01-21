a2dbc0e0bb802ec1d0971e3c9bd84826d86f0abf
elinakheirkhah-cyber <elinakheirkhah@gmail.com>
2026-01-21 11:05:53 +0100

    --

---
+++ alienspawn.h
@@ -0,0 +1,37 @@
+/*
+ * alien.h
+ *
+ *  Created on: 16. jan. 2026
+ *      Author: elina
+ */
+
+#ifndef ALIEN_H_
+#define ALIEN_H_
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+#define MAX_ALIENS 5
+#define ALIEN_HEIGHT 10
+#define ALIEN_WIDTH 10
+#define DEFAULT_SPEED 2
+#define MOVE_TICKS 1000
+
+
+typedef struct{
+	int16_t x,y;
+	int16_t active;
+	int16_t speed;
+	int16_t tick;
+	int16_t dx;
+	int16_t dy;
+}alienStruct;
+
+void draw_alien(uint16_t x, uint16_t y);
+void initAlien();
+void spawnAlien(uint8_t grid_width);
+
+void delete_alien(alienStruct *aliens);
+void updateAlien(uint8_t grid_width, uint8_t *bounce);
+
+
+
+#endif /* ALIEN_H_ */
