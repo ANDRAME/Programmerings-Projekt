efe5712cde5e10fd01c0ce175b480d49fed161cc
elinakheirkhah-cyber <elinakheirkhah@gmail.com>
2026-01-22 11:20:06 +0100

    Add LOSEscreen header file with function declarations

---
+++ LOSEscreen.h
@@ -0,0 +1,16 @@
+
+
+#ifndef LOSESCREEN_H_
+#define LOSESCREEN_H_
+
+
+
+void loseG (uint8_x, uint8_t y);
+void loseA(uint8_t x, uint8_t y);
+void losenM(uint8_t x, uint8_t y);
+void loseE(uint8_t x, uint8_t y);
+void loseOVER (uint8_t, x, uint8_t y);
+
+
+#endif /* LOSESCREEN_H_ */
+
